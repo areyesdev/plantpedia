422845648bd56acf33222169ff064857712443f1
Andres Reyes <areyesdev@gmail.com>
2022-03-18 20:38:08 -0500

    add loader image

--- components/PlantCollection.tsx
+++ components/PlantCollection.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link'
+import NextImage, { ImageLoaderProps } from 'next/image'
 import { Grid, GridProps } from '@ui/Grid'
 import { Typography } from '@ui/Typography'
 import { Button } from '@ui/Button'
@@ -49,12 +50,54 @@ export function PlantEntry({ plant, variant = 'square' }: PlantEntryType) {
   )
 }
 
+type ImageProps = {
+  layout: 'responsive' | 'intrinsic'
+  src: string
+  width: number
+  height?: never
+  aspectRatio: '1:1' | '4:3' | '16:9'
+  fit: 'pad' | 'crop' | 'fill' | 'none' | 'scale'
+}
+
+function Image({ layout, src, width, aspectRatio, fit = 'scale' }: ImageProps) {
+  const height = calcAspectRatio(aspectRatio, width)
+  const loader = (args: ImageLoaderProps): string => {
+    const loaderHight = calcAspectRatio(aspectRatio, args.width)
+    return `${args.src}?w=${args.width}&h=${loaderHight}&fit=${fit}`
+  }
+  return (
+    <NextImage
+      layout={layout}
+      src={src}
+      width={width}
+      height={height}
+      loader={loader}
+    />
+  )
+}
+
+const aspectRatioToRatio = {
+  '1:1': 1,
+  '4:3': 3 / 4,
+  '16:9': 9 / 16,
+}
+function calcAspectRatio(aspectRatio: '1:1' | '4:3' | '16:9', width: number) {
+  const ratio = aspectRatioToRatio[aspectRatio]
+  return Math.floor(width * ratio)
+}
+
 export function PlantEntrySquare({ image, plantName, slug }: Plant) {
   return (
     <Link href={`/entry/${slug}`}>
       <a title={`Go to ${plantName}`}>
         <div className="opacity-95 hover:opacity-100">
-          <img src={image.url} width={460} />
+          <Image
+            layout="responsive"
+            src={image.url}
+            width={460}
+            aspectRatio="1:1"
+            fit="crop"
+          />
           <div className="p-4">
             <Typography variant="h4" className="break-words">
               {plantName}
