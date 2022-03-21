1ced1dd333f8cdf16a558911039bfbf9ff6f8094
Andres Reyes <areyesdev@gmail.com>
2022-03-21 15:41:27 -0500

    add Language choice via Next.js API

---
+++ components/Header/TopArea.tsx
@@ -0,0 +1,39 @@
+import { useRouter } from 'next/router'
+
+import { Grid } from '@ui/Grid'
+import { Typography } from '@ui/Typography'
+import { Button } from '@ui/Button'
+
+export function TopArea() {
+  const { locales, locale } = useRouter()
+  if (locales == undefined || locale == undefined) {
+    return null
+  }
+  return (
+    <Grid container justify="space-between">
+      <Grid item></Grid>
+      <Grid item>
+        <Typography variant="body2" component="span" className="pr-3">
+          Language:
+        </Typography>
+        {locales.map((loc) => (
+          <form
+            action="/api/language"
+            method="POST"
+            key={loc}
+            className="inline-block"
+          >
+            <input type="hidden" name="preferredLocale" value={loc} />
+            <Button
+              variant={loc === locale ? 'outlined' : 'text'}
+              className="ml-1"
+              type="submit"
+            >
+              {loc}
+            </Button>
+          </form>
+        ))}
+      </Grid>
+    </Grid>
+  )
+}
